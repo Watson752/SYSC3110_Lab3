bb8b58e2db49ff40c51c6565c0f1a617f7135764
Watson752 <119341209+Watson752@users.noreply.github.com>
2025-09-19 15:17:39 -0400

    Added AddressBook class with add/remove methods

--- SYSC3110_Lab1/src/AddressBook.java
+++ SYSC3110_Lab1/src/AddressBook.java
@@ -1,13 +1,18 @@
 import java.util.ArrayList;
 
 public class AddressBook {
-    private ArrayList<BuddyInfo> AddressBook = new ArrayList<BuddyInfo>;
+    private ArrayList<BuddyInfo> AddressBook = new ArrayList<BuddyInfo>();
 
-    public void addBuddy(BuddyInfo buddy)(){
+    public void addBuddy(BuddyInfo buddy){
         AddressBook.add(buddy);
     }
 
-    public void removeBuddy(BuddyInfo buddy)(){
+    public void removeBuddy(BuddyInfo buddy){
         AddressBook.remove(buddy);
     }
+
+    public static void main(String[] args) {
+        System.out.println("Address book running..");
+    }
+
 }
